d7fd86e9b6d740e21edd1282ef1a847628461bfa
alessia vinnik <alessia.vinnik@student.howest.be>
2023-04-10 13:24:53 +0200

    navigation from login to homepage done

--- App.tsx
+++ App.tsx
@@ -2,14 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { LabStack } from './screens/LabStack';
-
+import Login from './screens/Login';
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <Text>This is App.tsx</Text>
-      <LabStack/>
+      <Login/>
     </NavigationContainer>
   );
 }
---
+++ screens/Login.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { View, Text, Alert, StyleSheet } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { useNavigation } from '@react-navigation/native';
+import { LabStack } from './LabStack';
+
+export default () => {
+  const [qrData, setQrData] = useState<string | null>(null);
+  const navigation = useNavigation();
+
+  const handleBarCodeScanned = ({ data }: { data: string }) => {
+    if (data === 'My_password_is_1234') {
+      setQrData(data);
+      Alert.alert('Success', 'Login successful!');
+    }
+    else {
+      Alert.alert('Error', 'Incorrect QR code, please try again');
+    }
+  };
+
+  if (qrData) {
+    return <LabStack/>;
+  }
+  else{
+    return(
+      <View style={styles.generic}>
+        <Text style={styles.header}>Scan the QR code</Text>
+        <BarCodeScanner
+            onBarCodeScanned={handleBarCodeScanned}
+            style={{ height: 200, width: 200 }}
+        />
+        <Text style={styles.text}>Scanning...</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  generic:{
+    flex: 1,
+    backgroundColor: "#264653",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  header:{
+    marginBottom: 20,
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+
+  text:{
+    marginTop: 20,
+  }
+})
